3f0d70f98d5dad238b7431cdd448a51085269049
Takeshi Kawasaki <72239760+TakeshiKawasaki@users.noreply.github.com>
2026-05-23 00:33:07 +0900

    Add files via upload

---
+++ Lecture5/analyze.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <iostream>
+#include <fstream>
+#include <cfloat>
+
+#define temp 1.0 //parameter: you can change this for changing temperature.
+#define dt 0.01
+#define ensemble 1000
+#define window 39
+#define dim 3
+//using namespace std;
+
+void ini(double *dr2,double *corr){
+  for(int i=0;i<window;i++){
+    dr2[i]=0.0;
+    corr[i]=0.0;
+  }
+}
+
+void input(double (*x)[dim],double (*v)[dim],double *t){
+  char filename[128];
+  std::ifstream file;
+  sprintf(filename,"coord_dt%.3fT%.3f.dat",dt,temp);
+  file.open(filename);
+  int asize=ensemble*window;
+  for(int i=0;i<asize;i++){
+    file >> t[i] >> x[i][0] >> x[i][1] >> x[i][2];
+  }
+  file.close();
+
+  sprintf(filename,"vel_dt%.3fT%.3f.dat",dt,temp);
+  file.open(filename);
+  for(int i=0;i<asize;i++){
+    file >> t[i] >> v[i][0] >> v[i][1] >> v[i][2];
+    // std::cout << t[i] <<"\t"<<v[0][i]<<"\t"<<v[1][i]<<"\t"<<v[2][i]<<std::endl;
+  }
+  file.close();
+}
+
+void output(double *t,double *dr2,double *corr){
+  char filename[128];
+  std::ofstream file;
+  sprintf(filename,"msd_dt%.3fT%.3f.dat",dt,temp);
+  file.open(filename);
+  for(int i=1;i<window;i++)
+    file<<t[i]-t[0]<<"\t"<<dr2[i]<<std::endl;
+  file.close();
+
+  sprintf(filename,"corr_dt%.3fT%.3f.dat",dt,temp);
+  file.open(filename);
+  for(int i=1;i<window;i++)
+    file<<t[i]-t[0]<<"\t"<<corr[i]<<std::endl;
+  file.close();
+}
+
+void analyze(double (*x)[dim],double (*v)[dim],double *t,double *dr2,double *corr){
+  double dx[dim],corr_x[dim];
+  for(int i=0;i<ensemble;i++)
+    for(int j=0;j<window;j++){
+      for(int k=0;k<dim;k++){
+	      dx[k]=(x[j+window*i][k]-x[window*i][k]);
+	      corr_x[k]=v[j+window*i][k]*v[window*i][k];
+	      dr2[j]+=(dx[k]*dx[k])/ensemble;
+	      corr[j]+=(corr_x[k])/ensemble;
+      }
+    }
+}
+
+int main(){
+  double t[ensemble*window],dr2[window],corr[window];
+  int i,j;
+  double  (*x)[dim] = new double[ensemble*window][dim];
+  double  (*v)[dim] = new double[ensemble*window][dim];
+
+  ini(dr2,corr);
+  input(x,v,t);
+  analyze(x,v,t,dr2,corr);
+  output(t,dr2,corr);
+  delete[] x;
+  delete[] v;
+  return 0;
+}
--- Lecture5/readme.md
+++ Lecture5/readme.md
@@ -1,1 +1,20 @@
+## 第5回講義動画（NUSSリンク） <bf>
+[https://nuss.nagoya-u.ac.jp/s/w5LaEG3ARL52Lyc](https://www.dropbox.com/scl/fi/nxfj84hoaggu2vaoo10mg/0520_3-_-5.mp4?rlkey=8l9081longtsw7a5v7bk2jthw&dl=0)
+  
+## 第５回アンケートQ&A集 <bf>
+https://irradiated-pot-667.notion.site/6-Q-A-37c976766d334f43b73eb0ce0f9b94a6?pvs=4
+
+## コードについての補足：
+#### BM.h:
+Box Muller法による分散1の正規乱数 `gaussian rand()` を生成する関数が格納されたヘッダファイル．次のlangevin.cppの冒頭で宣言している．
+`#include "BM.h"`
+#### langevin.cpp: 
+ランジュバン方程式を解くプログラム．デフォルトでは時刻10までの軌跡を1000アンサンブル生成する．パラメータtempは冒頭に定義．適宜変えること．
+以下の作図プログラムを用いる際は，`temp=1.0,0.1,0,0.01` の計算を行い軌道・速度データを生成する必要がある．
+
+#### analyze.cpp: 
+平均二乗変位と速度相関関数の解析プログラム．langevin.cppとtempのパラメータを揃える必要がある点に注意．以下の作図プログラムをそのまま使う場合は，，`temp=1.0,0.1,0,0.01`  の解析を行う必要がある．
+
+#### msd.py，corr_vel.py: 
+作図プログラム．`temp=1.0,0.1,0,0.01`  の解析結果(平均二乗変位と速度相関関数)を用意する必要がある．
 
